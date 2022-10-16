fcf130568ae04cca6c36fd16c8404ea2e918cef7
SamanthaMcElhinney <mcelhinneysamantha@gmail.com>
2022-10-16 13:54:24 -0700

    Add homework variables

--- variables.js
+++ variables.js
@@ -2,5 +2,5 @@ var profession = "software_developer"
 console.log(profession)
 var zipcode = 98258
 console.log(zipcode)
-homeworkCompleted = false
+var homeworkCompleted = false
 console.log(homeworkCompleted)
